9ae0ef8f287252221ccb047439413e40adae0a1d
shawnkoon <shawn9405@gmail.com>
2018-11-18 23:38:00 -0800

    Add losing collider with trigger to render game over scene

---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseCollider : MonoBehaviour {
+    private string gameOverSceneName = "GameOver";
+    private void OnTriggerEnter2D(Collider2D other) => SceneManager.LoadScene(gameOverSceneName);
+}
