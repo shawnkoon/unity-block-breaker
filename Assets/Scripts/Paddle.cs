22ba56428a3585dda74ae101f7a60dbe0cf5ca9c
Shawn Choi <shawn.choi@us.imshealth.com>
2018-11-21 22:13:53 -0800

    Add paddle movement && minor refactor on level scene

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    [SerializeField]
+    private float xUnitLength = 16f;
+    [SerializeField]
+    private float minUnitLength = 1f;
+    [SerializeField]
+    private float maxUnitLength = 15f;
+
+    // Update is called once per frame
+    void Update () {
+        var mouseX = Input.mousePosition.x / Screen.width * this.xUnitLength;
+        Vector2 paddlePosition = new Vector2(transform.position.x, transform.position.y);
+        paddlePosition.x = Mathf.Clamp(mouseX, this.minUnitLength, this.maxUnitLength);
+
+        transform.position = paddlePosition;
+    }
+}
